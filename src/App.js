c3e165ce09fb1a9dbce738fb30bf3078e4fef2cd
duc.tt <duc.tt@teko.vn>
2021-11-12 12:05:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { builder, renderPage } from "@teko-builder/builder-client";
+import {
+  componentDict,
+  ThemeProvider,
+  generateTheme,
+} from "@teko-builder/pb-base-elements";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+
+export const themeApp = generateTheme({
+  primary: "#258DFF",
+  secondary: "#FF2642",
+});
 
 function App() {
+  const location = useLocation();
+  const [page, setPage] = useState();
+  const fetchPage = async () => {
+    const slug = location.pathname.substring(1);
+    const { page: _page } = await builder.getPublicPage({
+      slug: slug ? slug : "/",
+      device: "desktop",
+    });
+    setPage(_page);
+  };
+  useEffect(() => {
+    builder.init("vnpay", {
+      env: "dev",
+    });
+    fetchPage();
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      {page?.pbConfig && renderPage(page?.pbConfig, componentDict)}
+    </ThemeProvider>
   );
 }
 
